b9fac2d7572bde8e8a1891896a59477f169631d5
Alexey Bilay <alexey.bilay@gmail.com>
2016-05-11 16:58:17 +0800

    ShiftType money settings

---
+++ app/src/main/java/com/snake/salarycounter/fragments/ShowShiftType/ShiftTypeMoneyFragment.java
@@ -0,0 +1,107 @@
+package com.snake.salarycounter.fragments.ShowShiftType;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.snake.salarycounter.R;
+import com.snake.salarycounter.events.TextEvent;
+import com.snake.salarycounter.models.ShiftType;
+import com.snake.salarycounter.watchers.TextValidator;
+
+import java.math.BigDecimal;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import de.greenrobot.event.EventBus;
+
+public class ShiftTypeMoneyFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "_id";
+
+    private long mId;
+
+    private ShiftType st;
+
+    @Bind(R.id.shift_type_money_is_count_hours) CheckBox isCountHours;
+    @OnCheckedChanged(R.id.shift_type_money_is_fixed_price) void onIsCountHoursChecked(boolean checked) {
+        st.isCountHours = checked;
+        st.save();
+    }
+
+    @Bind(R.id.shift_type_money_is_average_price) CheckBox isAveragePrice;
+    @OnCheckedChanged(R.id.shift_type_money_is_fixed_price) void onIsAverageChecked(boolean checked) {
+        st.isAveragePrice = checked;
+        st.save();
+    }
+
+    @Bind(R.id.shift_type_money_is_fixed_price) CheckBox isFixedPrice;
+    @OnCheckedChanged(R.id.shift_type_money_is_fixed_price) void onIsFixedPriceChecked(boolean checked) {
+        fixedPrice.setEnabled(checked);
+        st.isFixedPrice = checked;
+        st.save();
+    }
+
+    @Bind(R.id.shift_type_money_fixed_price) EditText fixedPrice;
+    @Bind(R.id.shift_type_money_additional_price) EditText additionalPrice;
+    @Bind(R.id.shift_type_money_multiplier) EditText multiplier;
+
+    public ShiftTypeMoneyFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mId = getArguments().getLong(ARG_PARAM1);
+            st = ShiftType.getById(mId);
+        }
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_shift_type_money, container, false);
+        ButterKnife.bind(this, v);
+
+        isCountHours.setChecked(st.isCountHours);
+        isAveragePrice.setChecked(st.isAveragePrice);
+        isFixedPrice.setChecked(st.isFixedPrice);
+        fixedPrice.setText(st.fixedPrice.toString());
+        fixedPrice.setEnabled(st.isFixedPrice);
+        fixedPrice.addTextChangedListener(new TextValidator(getActivity(), fixedPrice, st.getId()));
+        additionalPrice.setText(st.additionalPrice.toString());
+        additionalPrice.addTextChangedListener(new TextValidator(getActivity(), additionalPrice, st.getId()));
+        multiplier.setText(st.multiplier.toString());
+        multiplier.addTextChangedListener(new TextValidator(getActivity(), multiplier, st.getId()));
+        return v;
+    }
+
+    public void onEvent(TextEvent event){
+        if(st.getId() == event.mId) {
+            switch (event.mTextEditId) {
+                case R.id.shift_type_money_fixed_price:
+                    st.fixedPrice = new BigDecimal(event.mValue);
+                    st.save();
+                    break;
+                case R.id.shift_type_money_additional_price:
+                    st.additionalPrice = new BigDecimal(event.mValue);
+                    st.save();
+                    break;
+                case R.id.shift_type_money_multiplier:
+                    st.multiplier = new BigDecimal(event.mValue);
+                    st.save();
+                    break;
+            }
+        }
+    }
+}
