db1812b2ace3b7f83c9bbc292451bb618ad56f53
Alexey Bilay <alexey.bilay@gmail.com>
2017-02-07 19:13:56 +0700

    Fill calendar with same shift type for N days

--- app/src/main/java/com/snake/salarycounter/fragments/Calendar/CalendarFragment.java
+++ app/src/main/java/com/snake/salarycounter/fragments/Calendar/CalendarFragment.java
@@ -1,5 +1,6 @@
 package com.snake.salarycounter.fragments.Calendar;
 
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -8,6 +9,10 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
@@ -19,8 +24,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
+import com.activeandroid.content.ContentProvider;
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.mikepenz.iconics.IconicsDrawable;
@@ -33,6 +41,7 @@ import com.snake.salarycounter.events.SwitchEvent;
 import com.snake.salarycounter.fragments.PayslipFragment;
 import com.snake.salarycounter.models.Day;
 import com.snake.salarycounter.models.ShiftType;
+import com.snake.salarycounter.watchers.TextValidator;
 
 import org.joda.time.DateTime;
 
@@ -182,6 +191,82 @@ public class CalendarFragment extends Fragment {
                                                 .show();
                                         break;
                                     case 1: // set N days
+                                        boolean wrapInScrollView = true;
+                                        MaterialDialog nDaysDialog = new MaterialDialog.Builder(getContext())
+                                                //.title(R.string.n_day_shift_type)
+                                                .customView(R.layout.dialog_n_days_shift_type, wrapInScrollView)
+                                                .positiveText(R.string.activity_dialog_accept)
+                                                .negativeText(R.string.activity_dialog_decline)
+                                                .canceledOnTouchOutside(false)
+                                                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                                    @Override
+                                                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                                        Spinner spShiftType = (Spinner) dialog.getCustomView().findViewById(R.id.spinner);
+                                                        ShiftType shiftType = ShiftType.getByPosition(spShiftType.getSelectedItemPosition());
+                                                        EditText etDays = (EditText) dialog.getCustomView().findViewById(R.id.n_days_shift_type);
+                                                        if(etDays.getText().length() > 0) {
+                                                            int tmpDays = Integer.valueOf(etDays.getText().toString());
+                                                            DateTime tmpDate = new DateTime(mDate);
+
+                                                            while (tmpDays > 0) {
+                                                                Day d = Day.getByDate(tmpDate.toDate());
+
+                                                                if (d != null) {
+                                                                    d.shiftType = shiftType;
+                                                                    d.save();
+                                                                } else {
+                                                                    d = new Day(tmpDate.toDate());
+                                                                    d.shiftType = shiftType;
+                                                                    d.save();
+                                                                }
+
+                                                                caldroidFragment.setBackgroundDrawableForDate(new ColorDrawable(d.shiftType.color), tmpDate.toDate());
+
+                                                                tmpDate = tmpDate.plusDays(1);
+                                                                tmpDays--;
+                                                            }
+                                                            caldroidFragment.refreshView();
+                                                        }
+                                                    }
+                                                })
+                                                .build();
+
+                                        EditText etDays = (EditText) nDaysDialog.getCustomView().findViewById(R.id.n_days_shift_type);
+                                        etDays.addTextChangedListener(new TextValidator(getActivity(), etDays, 0));
+                                        etDays.setText("5");
+
+                                        final Spinner mySpinner = (Spinner) nDaysDialog.getCustomView().findViewById(R.id.spinner);
+                                        mySpinner.setAdapter(new SimpleCursorAdapter(getActivity(),
+                                                android.R.layout.simple_expandable_list_item_1,
+                                                null,
+                                                new String[] { "name" },
+                                                new int[] { android.R.id.text1 },
+                                                0));
+
+                                        getActivity().getSupportLoaderManager().initLoader(0, null, new LoaderManager.LoaderCallbacks<Cursor>() {
+                                            @Override
+                                            public Loader<Cursor> onCreateLoader(int arg0, Bundle cursor) {
+                                                return new CursorLoader(
+                                                        getActivity(),
+                                                        ContentProvider.createUri(ShiftType.class, null),
+                                                        null,
+                                                        null,
+                                                        null,
+                                                        "weight ASC"
+                                                );
+                                            }
+
+                                            @Override
+                                            public void onLoadFinished(Loader<Cursor> arg0, Cursor cursor) {
+                                                ((SimpleCursorAdapter)mySpinner.getAdapter()).swapCursor(cursor);
+                                            }
+
+                                            @Override
+                                            public void onLoaderReset(Loader<Cursor> arg0) {
+                                                ((SimpleCursorAdapter)mySpinner.getAdapter()).swapCursor(null);
+                                            }
+                                        });
+                                        nDaysDialog.show();
                                         break;
                                     case 2: // set with template
                                         break;
